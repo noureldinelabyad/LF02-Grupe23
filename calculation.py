932604781baf259c87f7274b4c04dd7881df9997
Maximilian Sachs <Maximilian.Lukas.Sachs@outlook.de>
2024-07-03 18:03:13 +0200

    added unittest + match case

--- calculation.py
+++ calculation.py
@@ -3,20 +3,29 @@ from Units import Unit
 
 
 def calculate_amount_by_distance(distance, unit):
-    if unit.lower() == Unit.KILOMETER.value:
-        amount = distance * config.KILOMETER_RATE
-    else:
-        amount = distance * config.KILOMETER_RATE / 1000
-    return amount
+    match unit.lower():
+        case Unit.KILOMETER.value:
+            return distance * config.KILOMETER_RATE
+        case Unit.METERS.value:
+            return distance * config.KILOMETER_RATE / 1000
+        case _:
+            print("Fehlerhafte Eingabe! Bitte Kilometer oder Meter angeben.")
 
 
 def calculate_amount_by_time(time, unit):
-    if unit.lower() == Unit.HOURS.value:
-        amount = time * config.MINUTE_RATE * 60
-    else:
-        amount = time * config.MINUTE_RATE
-    return amount
+    match unit.lower():
+        case Unit.HOURS.value:
+            return time * config.MINUTE_RATE * 60
+        case Unit.MINUTES.value:
+            return time * config.MINUTE_RATE
+        case _:
+            print("Fehlerhafte Eingabe! Bitte Minuten oder Stunden angeben.")
 
 
 def apply_discount(amount, code):
-    return amount * (1 - config.DISCOUNTS.get(code, 0))
+    discount_amount = config.DISCOUNTS.get(code)
+    if discount_amount is None:
+        print("Rabattcode Fehlerhaft!")
+        return amount
+    else:
+        return amount * (1 - discount_amount)
